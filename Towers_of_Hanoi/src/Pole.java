253a24ee95e2754322901fa474406b2348c02eb6
Daniel Krinke <krinked@gmu.edu>
2017-02-20 13:00:33 -0500

    Implemented Pole and Ring class. Added a stack interface for Pole.

--- Towers_of_Hanoi/src/Pole.java
+++ Towers_of_Hanoi/src/Pole.java
@@ -1,19 +1,64 @@
+import java.util.ArrayList;
+
 /**
  * Created by daniel on 2/20/17.
  */
-public class Pole {
+interface stack {
+    boolean push(Ring size);
+    boolean pop();
+    Ring top();
+    int count();
+}
+
+public class Pole implements stack {
 
     private int id = -1;
+    private ArrayList<Integer> nextToList;
+    private ArrayList<Ring> stack;
 
-    public Pole(int id){
+    public Pole(int id) {
         this.setId(id);
+        this.nextToList = new ArrayList<Integer>();
+        this.stack = new ArrayList<Ring>();
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int size) {
-        this.id = size;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Integer> isNextTo() {
+        return this.nextToList;
+    }
+
+    public void setIsNextTo(int id) {
+        nextToList.add(id);
+    }
+
+    public boolean addRing(Ring ring) {
+        return this.push(ring);
+    }
+
+    public void removeFromTop() {
+        this.pop();
+    }
+
+    public boolean push(Ring size) {
+        return stack.add(size);
+    }
+
+    public boolean pop() {
+        return stack.remove(this.top());
+    }
+
+    public Ring top() {
+        return this.stack.get(this.count()-1);
+    }
+
+    public int count() {
+        return this.stack.size();
     }
 }
--- Towers_of_Hanoi/src/Ring.java
+++ Towers_of_Hanoi/src/Ring.java
@@ -5,6 +5,8 @@ public class Ring {
 
     private int size = -1;
     private String color = "";
+    private Ring onTopOf = null;
+    private Pole onPole = null;
 
     public Ring(int size, String color){
         this.setSize(size);
@@ -19,11 +21,37 @@ public class Ring {
         return size;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    public boolean setSize(int size) {
+        boolean valueChanged = false;
+        if(size >= 0){
+            this.size = size;
+            valueChanged = true;
+        }
+        return valueChanged;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public boolean setColor(String color) {
+        boolean valueChanged = false;
+        if(color != null){
+            this.color = color;
+            valueChanged = true;
+        }
+        return valueChanged;
+    }
+
+    public boolean moveTo(Pole pole) {
+        if(pole.addRing(this)) {
+            this.onPole = pole;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean onTopOf(Ring ring) {
+        if(ring.getSize() < this.getSize()) {
+            this.onTopOf = ring;
+            return true;
+        }
+        return false;
     }
 }
