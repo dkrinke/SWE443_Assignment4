d1188508e13d2ee7fab128f3aa89a85968f7c933
henryapez <hlopezgu@gmu.edu>
2017-02-20 16:14:40 -0500

    Added fields to House class -Henry

--- Mancala/src/House.java
+++ Mancala/src/House.java
@@ -1,4 +1,42 @@
 
 public class House {
 
+	private int stoneCount;
+	private boolean myTurn;
+	private String name;
+	
+	
+	
+	public void setStoneCount(int count){
+		if(count < 0){
+			throw new IllegalArgumentException("Player cannout have negative stones!");
+		}
+		this.stoneCount = count;
+	}
+	public int getStoneCount(){
+		return this.stoneCount;
+	}
+	public int incrementStoneCount(){
+		this.stoneCount++;
+	}
+	
+	
+	public void setMyTurn(boolean turn){
+		if(turn==null){
+			throw new IllegalArgumentException("Player's turn cannot be null!");
+		}
+		this.myTurn = turn;
+	}
+	public boolean isMyTurn(){
+		return myTurn;
+	}
+	
+	
+	
+	public String getName(){
+		return this.name;
+	}
+	public void setName(){
+		this.name;
+	}
 }
