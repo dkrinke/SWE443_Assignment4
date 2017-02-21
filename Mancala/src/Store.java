1ffba7cc86c33ba7137ef27f1f3c7aa8a5c8c463
Vincent Liu <liuvincent19915@gmail.com>
2017-02-20 19:55:13 -0500

    Added contructors and stuff to Store
    Added constructors.
    Added opposite store.
    Added getters and setters for opposite store

--- Mancala/src/Store.java
+++ Mancala/src/Store.java
@@ -1,4 +1,22 @@
 
 public class Store extends House{
-
+	private Store opposite;
+	public Store(){
+		super(); 
+	}
+	public Store(int initStones){
+		super(initStones);
+	}
+	public Store(int initStones, boolean turn){
+		super(initStones,turn);
+	}
+	public Store getOpposite(){
+		return this.opposite();
+	}
+	public void setOpposite(Store op){
+		this.opposite = op;
+		if(op.opposite != this){
+			op.setOpposite(this);
+		}
+	}
 }
