4bd1da422e21cd51f2869c6b74da87dd54ca3f14
Vincent Liu <liuvincent19915@gmail.com>
2017-02-20 19:57:36 -0500

    Fixed constructors and changed setOpposite
    Set opposite to null in constructors.
    changed setOpposite to handle a null argument

--- Mancala/src/Store.java
+++ Mancala/src/Store.java
@@ -3,19 +3,22 @@ public class Store extends House{
 	private Store opposite;
 	public Store(){
 		super(); 
+		this.setOpposite(null);
 	}
 	public Store(int initStones){
 		super(initStones);
+		this.setOpposite(null);
 	}
 	public Store(int initStones, boolean turn){
 		super(initStones,turn);
+		this.setOpposite(null);
 	}
 	public Store getOpposite(){
 		return this.opposite();
 	}
 	public void setOpposite(Store op){
 		this.opposite = op;
-		if(op.opposite != this){
+		if(op != null && op.opposite != this){
 			op.setOpposite(this);
 		}
 	}
